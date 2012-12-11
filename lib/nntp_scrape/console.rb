43acceb8be4e2a8429ffca7f98164fce4883d8aa
Scott Fleckenstein <nullstyle@gmail.com>
2012-12-10 21:49:22 -0800

    Add simpler prompt and other helpers to console

--- lib/nntp_scrape/console.rb
+++ lib/nntp_scrape/console.rb
@@ -2,20 +2,38 @@ require 'pry'
 
 module NntpScrape
   class Console 
+    include Commands
+    
     def initialize(client)
       @client = client
     end
     
     def start
-      binding.pry
+      my_prompt = [ 
+        proc { |obj, *| ">> " },
+        proc { |obj, *| "*> "} 
+      ]
+      binding.pry :quiet => true, :cli => true, :prompt => my_prompt
+    end
+    
+    def run(klass, *params)
+      cmd = klass.new(*params)
+      @client.run(cmd)
+      cmd
     end
     
     def short(cmd, *params)
       @client.run_short(cmd, *params)
+    rescue Errno::EPIPE, Errno::ECONNRESET, OpenSSL::SSL::SSLError
+      @client.open
+      retry
     end 
        
     def long(cmd, *params)
       @client.run_long(cmd, *params)
+    rescue Errno::EPIPE, Errno::ECONNRESET, OpenSSL::SSL::SSLError
+      @client.open
+      retry
     end
   end
 end
