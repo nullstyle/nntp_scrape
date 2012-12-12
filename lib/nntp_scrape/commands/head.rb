8a2e536b147b9ba6b91d64ba39665dd8a6d57678
Scott Fleckenstein <nullstyle@gmail.com>
2012-12-12 10:58:39 -0800

    Changes header parsing such that headers being split onto multiple lines are handles

--- lib/nntp_scrape/commands/head.rb
+++ lib/nntp_scrape/commands/head.rb
@@ -18,11 +18,20 @@ module NntpScrape
         
         @message_id = status_line.split.last
         @data = {}
+        last_header = nil
         lines.each do |line|
           key, value = *line.split(": ", 2)
           
-          @data[key] ||= ""
-          @data[key] += value
+          # in the case that this line doesn't have a a colon, we append to the
+          # last written header
+          if value.blank?
+            @data[last_header] += key
+          else
+            @data[key] ||= ""
+            @data[key] += value
+            last_header = key
+          end
+        
         end        
       end
     end
